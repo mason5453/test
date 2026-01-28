9784bd23835c30d20154d43ce215b16ab37b5e98
aiken <test@test.com>
2026-01-28 12:35:59 +0800

    Save

--- demo/tailwind_demo/tailwind.3.loadhtml.js
+++ demo/tailwind_demo/tailwind.3.loadhtml.js
@@ -6,7 +6,9 @@ async function loadHtml(url) {
 
 async function loadAllContent() {
   try {
-    const bodyHtml = await loadHtml('tailwind.4.body.html');
+    const bodyHeaderHtml = await loadHtml('tailwind.4.body.header.html');
+    const bodyBodyHtml   = await loadHtml('tailwind.4.body.body.html');
+    const bodyFooterHtml = await loadHtml('tailwind.4.body.footer.html');
     document.body.innerHTML = bodyHtml;
   } catch (err) {
     console.error('加载XML失败：', err);
