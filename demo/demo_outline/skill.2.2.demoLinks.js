15d151bed88c92989d2e5ca2b741cfc38b133ebf
aiken <test@test.com>
2026-01-28 15:44:44 +0800

    Save

--- demo/demo_outline/skill.2.2.demoLinks.js
+++ demo/demo_outline/skill.2.2.demoLinks.js
@@ -1,83 +1,80 @@
-
-
-// Demo URLs for each skill (expanded)
 const demoLinks = {
     // Programming Languages
-    "C#": "https://dotnetfiddle.net/",
-    "PHP": "https://3v4l.org/",
-    "JavaScript": "https://jsfiddle.net/",
-    "TypeScript": "https://www.typescriptlang.org/play",
-    "Python": "https://replit.com/languages/python3",
-    "Java": "https://www.jdoodle.com/online-java-compiler",
-    "C++": "https://www.onlinegdb.com/online_c++_compiler",
-    "Kotlin": "https://play.kotlinlang.org/",
+    "C#": null,
+    "PHP": null,
+    "JavaScript": null,
+    "TypeScript": null,
+    "Python": null,
+    "Java": null,
+    "C++": null,
+    "Kotlin": null,
     
     // Frontend Technologies
-    "HTML5": "https://codepen.io/pen/",
-    "CSS3": "https://codepen.io/pen/",
-    "SCSS": "https://sassmeister.com/",
-    "Bootstrap": "https://codeply.com/new",
-    "Tailwind CSS": "https://play.tailwindcss.com/",
-    "jQuery": "https://jsfiddle.net/",
-    "React": "https://codesandbox.io/s/new",
-    "Next.js": "https://stackblitz.com/fork/nextjs",
-    "Vue.js": "https://play.vuejs.org/",
-    "Angular": "https://stackblitz.com/fork/angular",
-    "React Native": "https://snack.expo.dev/",
-    "Ant Design": "https://codesandbox.io/s/antd-reproduction-template-6e93z",
+    "HTML5": null,
+    "CSS3": null,
+    "SCSS": null,
+    "Bootstrap": null,
+    "Tailwind CSS": null,
+    "jQuery": null,
+    "React": null,
+    "Next.js": null,
+    "Vue.js": null,
+    "Angular": null,
+    "React Native": null,
+    "Ant Design": null,
     
     // Backend Technologies
-    ".NET": "https://dotnetfiddle.net/",
-    "ASP.NET": "https://dotnetfiddle.net/",
-    "Node.js": "https://replit.com/languages/nodejs",
-    "WordPress": "https://wordpress.com/",
-    "GraphQL": "https://graphql.org/swapi-graphql",
+    ".NET": null,
+    "ASP.NET": null,
+    "Node.js": null,
+    "WordPress": null,
+    "GraphQL": null,
     
     // Databases
-    "MySQL": "https://www.db-fiddle.com/f",
-    "PostgreSQL": "https://www.db-fiddle.com/f",
-    "Redis": "https://redis.io/docs/ui/",
+    "MySQL": null,
+    "PostgreSQL": null,
+    "Redis": null,
     
     // Web Servers
-    "Apache": "https://httpd.apache.org/",
-    "Nginx": "https://nginx.org/en/",
-    "Tomcat": "https://tomcat.apache.org/",
+    "Apache": null,
+    "Nginx": null,
+    "Tomcat": null,
     
     // Version Control
-    "Git": "https://git-scm.com/",
-    "SVN": "https://subversion.apache.org/",
+    "Git": null,
+    "SVN": null,
     
     // IDEs & Editors
-    "Visual Studio Code (VSC)": "https://vscode.dev/",
-    "IntelliJ IDEA": "https://www.jetbrains.com/idea/",
-    "Android Studio": "https://developer.android.com/studio",
-    "Sublime Text": "https://www.sublimetext.com/",
+    "Visual Studio Code (VSC)": null,
+    "IntelliJ IDEA": null,
+    "Android Studio": null,
+    "Sublime Text": null,
     
     // DevOps & Tools
-    "Docker": "https://www.docker.com/playground/",
-    "Postman": "https://www.postman.com/",
-    "PowerShell": "https://learn.microsoft.com/en-us/powershell/",
+    "Docker": null,
+    "Postman": null,
+    "PowerShell": null,
     
     // Package Managers
-    "NPM": "https://www.npmjs.com/",
-    "PNPM": "https://pnpm.io/",
+    "NPM": null,
+    "PNPM": null,
     
     // AI & Machine Learning
-    "Machine learning": "https://colab.research.google.com/",
-    "Neural Networks (NN)": "https://playground.tensorflow.org/",
-    "AI frameworks": "https://pytorch.org/",
+    "Machine learning": null,
+    "Neural Networks (NN)": null,
+    "AI frameworks": null,
     
     // Productivity Tools
-    "Excel": "https://www.microsoft.com/en-us/microsoft-365/excel",
-    "Outlook": "https://outlook.live.com/",
-    "PowerPoint": "https://www.microsoft.com/en-us/microsoft-365/powerpoint",
+    "Excel": null,
+    "Outlook": null,
+    "PowerPoint": null,
     
     // Design & Prototyping
-    "Figma": "https://www.figma.com/",
+    "Figma": null,
     
     // Analytics & Tracking
-    "Google Analytics": "https://analytics.google.com/",
+    "Google Analytics": null,
     
     // Other
-    "Map technologies/APIs": "https://leafletjs.com/examples/quick-start/"
+    "Map technologies/APIs": null
 };
