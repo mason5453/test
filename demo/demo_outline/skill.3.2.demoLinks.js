8d1c012b68dd54b56f2b20546fac3ed15c9b7b38
aiken <test@test.com>
2026-02-02 13:42:32 +0800

    Save

--- demo/demo_outline/skill.3.2.demoLinks.js
+++ demo/demo_outline/skill.3.2.demoLinks.js
@@ -12,8 +12,8 @@ const demoLinks = {
     "typescript": baseUrl1 + "ts_demo/ts.png",
     "vbs":        baseUrl1 + "vb_demo/vb.png",
     // OS
-    "Windows":    baseUrl1 + "win_cmd_demo/win_cmd.html",
-    "Linux":      baseUrl1 + "shell_demo/shell.html",
+    "windows":    baseUrl1 + "win_cmd_demo/win_cmd.html",
+    "linux":      baseUrl1 + "shell_demo/shell.html",
 
     // Frontend Technologies
     "html5":         null,
