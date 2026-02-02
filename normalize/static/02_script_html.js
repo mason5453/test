364ed445f0c039bb8bb71ed0e1ac082bdfcdd775
aiken <test@test.com>
2026-02-02 18:02:06 +0800

    Save

--- normalize/static/02_script_html.js
+++ normalize/static/02_script_html.js
@@ -1,1 +1,32 @@
-async function loadHtml(t){const o=await fetch(t);if(!o.ok)throw new Error(`加载${t}失败`);return await o.text()}async function loadAllContent(){try{const t=await loadHtml("static/01_body.html");document.body.innerHTML=t;const o=await loadHtml("static/01_map_mos.html"),n=document.getElementById("mos");n?n.innerHTML=o:console.warn('未找到id="mos"的td元素')}catch(t){console.error("加载XML失败：",t)}}window.addEventListener("load",loadAllContent);
+async function loadHtml(url) {
+  const response = await fetch(url);
+  if (!response.ok) throw new Error(`加载${url}失败`);
+  return await response.text();
+}
+
+async function loadAllContent() {
+  try {
+    const bodyHtml = await loadHtml('static/01_body.html');
+    document.body.innerHTML = bodyHtml;
+
+    const mapHtml = await loadHtml('static/01_map_mos.html');
+    const mosTd = document.getElementById('mos');
+    if (mosTd) {
+      mosTd.innerHTML = mapHtml;
+      //if (mosTd.querySelector('#map_mos')) initMap('map_mos'); // 假设initMap接收地图容器ID
+
+
+      // await _start();
+    } else {
+      console.warn('未找到id="mos"的td元素');
+    }
+  } catch (err) {
+    console.error('加载XML失败：', err);
+  }
+}
+
+
+
+
+window.addEventListener('load', loadAllContent);
+
